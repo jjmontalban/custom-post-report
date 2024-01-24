b38537732dd835a108d83930eb8cbadf8c61edf5
JJMontalban <jjmontalban@gmail.com>
2024-01-24 18:39:19 +0100

    First commit

---
+++ includes/class-cpr-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://jjmontalban.github.io
+ * @since      1.0.0
+ *
+ * @package    Cpr
+ * @subpackage Cpr/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Cpr
+ * @subpackage Cpr/includes
+ * @author     JJMontalban <jjmontalban@gmail.com>
+ */
+class Cpr_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-cpr-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://jjmontalban.github.io
+ * @since      1.0.0
+ *
+ * @package    Cpr
+ * @subpackage Cpr/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cpr
+ * @subpackage Cpr/includes
+ * @author     JJMontalban <jjmontalban@gmail.com>
+ */
+class Cpr_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
